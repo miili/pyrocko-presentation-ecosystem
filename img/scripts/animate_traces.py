39a78465f2add8bea514249b7866f2434beb37d6
miili <marius.isken@gmail.com>
2018-02-12 11:57:44 +0100

    upd

--- img/scripts/animate_traces.py
+++ img/scripts/animate_traces.py
@@ -1,56 +1,81 @@
+import sys
 import numpy as num
-import matplotlib
 
-matplotlib.use("Agg")
-from pyrocko.gf import LocalEngine, DCSource, Target, HalfSinusoidSTF
+from pyrocko.gf import LocalEngine, DCSource, Target
 import matplotlib.pyplot as plt
 import matplotlib.animation as manimation
 
+km = 1e3
 
+nframes = 100
+trace_offset = 0.1
 
-# stf = HalfSinusoidSTF(0.1)
-dip = 15.
-rake = 5.
-strikes = num.linspace(-60., 20, 100)
-engine = LocalEngine(store_superdirs=['.'])
 store_id = 'qplayground_total_4_mr_full'
-east_shifts = num.linspace(0., 10000., 25)
-targets = [Target(store_id=store_id, east_shift=east_shift, north_shift=east_shift) for east_shift in east_shifts]
-nshift = 10000.
-depth = 10000.
-offset = 0.2
-ylim = len(targets)*offset
+engine = LocalEngine(store_superdirs=['.'])
 
-fig  = plt.figure()
-ax = fig.add_subplot(111)
+ls = num.linspace
+dips = ls(0, 360, nframes)
+rakes = ls(5., 5., nframes)
+strikes = ls(-180., 180, nframes)
+depths = ls(10*km, 10*km, nframes)
+east_shifts = ls(0., 10*km, 25)
+nshift = 10*km
+
+targets = [
+    Target(store_id=store_id, east_shift=east_shift, north_shift=east_shift)
+    for east_shift in east_shifts]
+
+ylim = len(targets)*trace_offset
+
+fig = plt.figure()
+ax = fig.add_subplot(111, facecolor='black')
 
 ax.set_xlim(1.4, 5)
 ax.set_ylim(-1., ylim)
 
-FFMpegWriter = manimation.writers['ffmpeg']
-metadata = dict(title='Seismodelic', artist='Matplotlib',
-                                comment='Movie support!')
-writer = FFMpegWriter(fps=10, metadata=metadata)
-
 lines = []
 for t in targets:
-    l, = ax.plot([], [], color='black', linewidth=0.75)
+    l, = ax.plot([], [],
+                 color='white',
+                 alpha=.8,
+                 linewidth=0.75,
+                 animated=True)
     lines.append(l)
 
 ax.set_xticks([])
 ax.set_yticks([])
 plt.subplots_adjust(left=0, right=1., bottom=0., top=1.)
-with writer.saving(fig, "seismodelic.mp4", 200):
-    for strike in strikes:
-        sources = [DCSource(
-            depth=depth, north_shift=nshift, strike=strike, dip=dip, rake=rake)]
-        response = engine.process(sources=sources, targets=targets)
 
-        trs = response.pyrocko_traces()
-        scale = num.max([tr.ydata for tr in trs])
-        for itr, tr in enumerate(trs):
-            tr.lowpass(4, 12.)
-            tr.highpass(4, .1)
-            lines[itr].set_data(tr.get_xdata(), tr.get_ydata()/scale +itr * offset)
+source = DCSource()
 
-        writer.grab_frame()
+def update_plot(iframe, *args):
+    source.depth = depths[iframe]
+    source.north_shift = nshift
+    source.strike = strikes[iframe]
+    source.dip = dips[iframe]
+    source.rake = rakes[iframe]
+
+    response = engine.process(
+        sources=[source],
+        targets=targets)
+
+    trs = response.pyrocko_traces()
+    scale = num.max([tr.ydata for tr in trs])
+
+    for itr, tr in enumerate(trs):
+        tr.lowpass(4, 12.)
+        tr.highpass(4, .1)
+        lines[itr].set_data(
+            tr.get_xdata(),
+            tr.get_ydata()/scale + itr * trace_offset)
+
+    return lines
+
+
+if __name__ == '__main__':
+    ani = manimation.FuncAnimation(
+        fig, update_plot, interval=50, frames=nframes, blit=True)
+    if len(sys.argv) > 1 and sys.argv[1] == 'save':
+        ani.save('traces.gif', dpi=80, writer='imagemagick')
+    else:
+        fig.show()
